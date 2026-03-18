0c760706d6bbf1b87ffe0ffcde4ef76e30e1f8cd
Prathameshmane1710 <96249504+Prathameshmane1710@users.noreply.github.com>
2026-03-18 17:50:43 -0400

    todo app final

--- todo_tasks/urls.py
+++ todo_tasks/urls.py
@@ -3,6 +3,12 @@ from .views import *
 
 urlpatterns = [
     path("addTask/", addTask , name="addTask"),
+    
     path("mark_as_done/<int:pk>/",mark_as_done,name="mark_as_done"),
     path("mark_as_undone/<int:pk>/",mark_as_undone,name="mark_as_undone"),
+    
+    path("edit_task/<int:pk>/",edit_task,name="edit_task"),
+
+    path("delete_taks/<int:pk>/",delete_task,name="delete_task"),
+
 ]
--- todo_tasks/views.py
+++ todo_tasks/views.py
@@ -20,3 +20,20 @@ def mark_as_undone(request,pk):
     task.is_completed=False
     task.save()
     return redirect("home")
+
+def edit_task(request,pk):
+    get_task = get_object_or_404(Task,pk=pk)
+    if request.method == "POST":
+        get_task.task = request.POST['task']
+        get_task.save()
+        return redirect("home")
+    else:
+        context = {
+            'get_task':get_task,
+        }
+        return render(request,'edit_task.html',context)
+
+def delete_task(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.delete()
+    return redirect("home")
