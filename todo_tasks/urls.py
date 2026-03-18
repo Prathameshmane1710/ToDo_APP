80a9ca92b7e53ac6ee176ba1d7f6086be3dd0dfc
Prathameshmane1710 <96249504+Prathameshmane1710@users.noreply.github.com>
2026-03-18 01:03:47 -0400

    Button Features Added

--- todo_tasks/urls.py
+++ todo_tasks/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("addTask/", addTask , name="addTask"),
+    path("mark_as_done/<int:pk>/",mark_as_done,name="mark_as_done"),
+    path("mark_as_undone/<int:pk>/",mark_as_undone,name="mark_as_undone"),
 ]
